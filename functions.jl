0d3cdc6f31a0a772cf4525d2babc5fb398ee0647
AlejandroDopico2 <alejandro.dopico2@udc.es>
2023-11-14 12:08:32 +0100

    Unit 7

--- functions.jl
+++ functions.jl
@@ -1,6 +1,8 @@
 using Statistics;
 using Flux;
 using Flux.Losses;
+using Random;
+using Flux:train!;
 
 function oneHotEncoding(feature::AbstractArray{<:Any,1}, classes::AbstractArray{<:Any,1})
     num_classes = length(classes)
@@ -166,8 +168,6 @@ function buildClassANN(numInputs::Int, topology::AbstractArray{<:Int,1}, numOutp
     return ann;
 end
 
-using Random
-
 function holdOut(N::Int, P::Float64)
     # Verify that P is a valid probability
     @assert ((P >= 0.0) && (P <= 1.0))
@@ -213,8 +213,26 @@ end
 N = 10
 train_indexes, val_indexes, test_indexes = holdOut(N, 0.2, 0.1)
 
+"""
+Calculates the confusion matrix and performance metrics for a multiclass classification problem.
 
-using Flux:train!
+# Arguments
+- `outputs::AbstractArray{Bool,2}`: A matrix of predicted outputs, where each row corresponds to a sample and each column corresponds to a class. The values should be binary (true/false) indicating whether the sample belongs to the corresponding class.
+- `targets::AbstractArray{Bool,2}`: A matrix of target outputs, where each row corresponds to a sample and each column corresponds to a class. The values should be binary (true/false) indicating whether the sample belongs to the corresponding class.
+- `weighted::Bool=true`: A boolean indicating whether to calculate the weighted or unweighted performance metrics. If `weighted=true`, the metrics are weighted by the number of samples in each class.
+
+# Returns
+A named tuple containing the following performance metrics:
+- `accuracy`: The overall accuracy of the classification.
+- `errorRate`: The overall error rate of the classification.
+- `sensitivity`: A vector of sensitivities (true positive rate) for each class.
+- `specificity`: A vector of specificities (true negative rate) for each class.
+- `precision`: A vector of precisions (positive predictive value) for each class.
+- `negative_predictive_value`: A vector of negative predictive values for each class.
+- `fScore`: A vector of F1 scores for each class.
+- `confusion_matrix`: The confusion matrix, where each row corresponds to a predicted class and each column corresponds to a true class.
+"""
+
 function trainClassANN(topology::AbstractArray{<:Int,1},  
             trainingDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}; 
             validationDataset::Tuple{AbstractArray{<:Real,2}, AbstractArray{Bool,2}}= 
@@ -225,140 +243,146 @@ function trainClassANN(topology::AbstractArray{<:Int,1},
             maxEpochs::Int=1000, minLoss::Real=0.0, learningRate::Real=0.01,  
             maxEpochsVal::Int=20, showText::Bool=false)
     
-    (trainingInputs, trainingTargets) = trainingDataset
-
-    # Check if validation and test sets are empty
-    is_val_empty = size(validationDataset[1]) == (0, 0)
-    is_test_empty = size(testDataset[1]) == (0, 0)
-
-    if !is_val_empty
-        validationInputs, validationTargets = validationDataset
-
-        @assert(size(validationInputs, 1) == size(validationTargets, 1))
-        @assert(size(trainingInputs, 2) == size(validationInputs, 2))
-        @assert(size(trainingTargets, 2) == size(validationTargets, 2))
-    end
-
-    if !is_test_empty
-        testInputs, testTargets = testDataset
-
-        @assert(size(testInputs, 1) == size(testTargets, 1))
-        @assert(size(trainingInputs, 2) == size(testInputs, 2))
-        @assert(size(trainingTargets, 2) == size(testTargets, 2))
-    end
+    # Split into inputs/targets
+    trainingInputs, trainingTargets = trainingDataset
+    validationInputs, validationTargets = validationDataset
+    testInputs, testTargets = testDataset
     
-    # We define the ANN
-    ann = buildClassANN(size(trainingInputs, 2), topology, size(trainingTargets, 2))
+    useValidation = length(validationInputs) > 0
+    useTest = length(testInputs) > 0
 
-    # Setting up the loss function to reduce the error
-    loss(model,x,y) = (size(y,1) == 1) ? Losses.binarycrossentropy(model(x),y) : Losses.crossentropy(model(x),y)
-
-    # Initialize variables for early stopping
-    bestValidationLoss = Inf
-    consecutiveEpochsWithoutImprovement = 0
-
-    # This vector is going to contain the losses on each training epoch
-    trainingLosses = Float32[]
-    validationLosses = Float32[]
-    testLosses = Float32[]
-
-    # Calculate the loss without training
-    trainingLoss = loss(ann, trainingInputs', trainingTargets')
-
-    if (!is_test_empty)
-        testLoss = loss(ann, testInputs', testTargets')
-        push!(testLosses, testLoss)
-    end
+    # Check that the targets corresponding to the inputs have the same number of samples
+    @assert (size(trainingInputs, 1) == size(trainingTargets, 1)) "Number of training inputs and targets do not match"
+    @assert (size(validationInputs, 1) == size(validationTargets, 1)) "Number of validation inputs and targets do not match"
+    @assert (size(testInputs, 1) == size(testTargets, 1)) "Number of test inputs and targets do not match"
     
-    if (!is_val_empty)
-        validationLoss = loss(ann, validationInputs', validationTargets')
-        push!(validationLosses, validationLoss)
+    if (useValidation)
+        @assert (size(trainingInputs, 2) == size(validationInputs, 2)) "Number of attributes for training and validation do not match"
+    end
+    if (size(testInputs, 1) != 0)
+        @assert (size(trainingInputs, 2) == size(testInputs, 2)) "Number of attributes for training and test do not match"
+    end
+   
+    # Build the network
+    nInputs, nOutputs = size(trainingInputs, 2), size(trainingTargets, 2)
+    ann = buildClassANN(nInputs, topology, nOutputs; transferFunctions)
+
+    if showText
+        println("ANN network built: $ann")
     end
 
-    if (showText)
-        if (!is_test_empty && !is_val_empty)
-            println("Epoch 0: Train Loss = ", trainingLoss, ", Validation Loss = ", validationLoss, ", Test Loss = ", testLoss)
-        elseif (!is_val_empty)
-            println("Epoch 0: Train Loss = ", trainingLoss, ", Validation Loss = ", validationLoss)
-        elseif (!is_test_empty)
-            println("Epoch 0: Train Loss = ", trainingLoss, ", Test Loss = ", testLoss)
-        else
-            println("Epoch 0: Train Loss = ", trainingLoss)
-        end
-    end
+    # Loss
+    loss(x,y) = (size(y, 1) == 1) ? Losses.binarycrossentropy(ann(x), y) : Losses.crossentropy(ann(x), y)
+    # Metric progress
+    trainingLosses = Array{Float32}(undef, 0)
+    validationLosses = Array{Float32}(undef, 0)
+    testLosses = Array{Float32}(undef, 0)
+    trainingAccs = Array{Float32}(undef, 0)
+    validationAccs = Array{Float32}(undef, 0)
+    testAccs = Array{Float32}(undef, 0)
 
-    # Store this one for checking the evolution.
-    push!(trainingLosses, trainingLoss)
+    # Train for n=max_epochs (at most) epochs
+    currentEpoch = 0
 
-    # Define the optimizer for the network
-    opt_state = Flux.setup(Adam(learningRate), ann)
-
-    # Start the training until it reaches one of the stop criteria
-    for numEpoch in 1:maxEpochs
-        # Train for one epoch
-        train!(loss, ann, [(trainingInputs', trainingTargets')], opt_state)
-
-        # Calculate the loss for this epoch
-        trainingLoss = loss(ann, trainingInputs', trainingTargets')
-        # Store it
-        push!(trainingLosses, trainingLoss)
-
-        if !is_test_empty
-            testLoss = loss(ann, testDataset[1]', testDataset[2]')
-            push!(testLosses, testLoss)
-        end
-
-        # If a validation set is provided, calculate the validation loss
-        if !is_val_empty
-            validationLoss = loss(ann, validationDataset[1]', validationDataset[2]')
-            push!(validationLosses, validationLoss)
-
-            # Check if validation loss improved
-            if validationLoss < bestValidationLoss
-                bestValidationLoss = validationLoss
-                consecutiveEpochsWithoutImprovement = 0
-                best_ann = deepcopy(ann)
-            else
-                consecutiveEpochsWithoutImprovement += 1
-            end
-
-            # Stop training if maxEpochsVal consecutive epochs without improvement are reached
-            if consecutiveEpochsWithoutImprovement >= maxEpochsVal
-                println("Early stopping after $numEpoch epochs.")
-                ann = best_ann  # Return the best ANN
-                break
-            end
+    # Calculate, store and print last loss/accuracy
+    function calculateMetrics()
+        
+        # Losses
+        trainingLoss   = loss(trainingInputs', trainingTargets')
+        validationLoss = (size(validationInputs, 1) != 0) ? loss(validationInputs', validationTargets') : 0
+        testLoss       = (size(testInputs, 1) != 0) ? loss(testInputs', testTargets') : 0
+     
+        # Accuracies
+        trainingOutputs   = ann(trainingInputs')
+        
+        validationAcc = 0
+        testAcc = 0
+        
+        if useValidation
+            validationOutputs = ann(validationInputs')
+            validationAcc = accuracy(validationOutputs', validationTargets)
         end
 
-        if (showText)
-            if (!is_test_empty && !is_val_empty)
-                println("Epoch ", numEpoch, ": Train Loss = ", trainingLoss, ", Validation Loss = ", validationLoss, ", Test Loss = ", testLoss)
-            elseif (!is_val_empty)
-                println("Epoch ", numEpoch, ": Train Loss = ", trainingLoss, ", Validation Loss = ", validationLoss)
-            elseif (!is_test_empty)
-                println("Epoch ", numEpoch, ": Train Loss = ", trainingLoss, ", Test Loss = ", testLoss)
-            else
-                println("Epoch ", numEpoch, ": Train Loss = ", trainingLoss)
-            end
+        if useTest
+            testOutputs = ann(testInputs')
+            testAcc = accuracy(testOutputs',       testTargets)
         end
         
+        trainingAcc   = accuracy(trainingOutputs',   trainingTargets)
+
+        # Update the history of losses and accuracies
+        push!(trainingLosses, trainingLoss)
+        push!(validationLosses, validationLoss)
+        push!(testLosses, testLoss)
+        push!(trainingAccs, trainingAcc)
+        push!(validationAccs, validationAcc)
+        push!(testAccs, testAcc)
+            
+        # Show text
+        if showText && (currentEpoch % 50 == 0)
+            println("Epoch ", currentEpoch, 
+                ": \n\tTraining loss: ", trainingLoss, ", accuracy: ", 100 * trainingAcc, 
+                "% \n\tValidation loss: ", validationLoss, ", accuracy: ", 100 * validationAcc, 
+                "% \n\tTest loss: ", testLoss, ", accuracy: ", 100 * testAcc, "%")
+        end
+        
+        return trainingLoss, trainingAcc, validationLoss, validationAcc, testLoss, testAcc
     end
 
-    # If a validation set is empty, the ANN to return is the one at the last cycle
-    if is_val_empty
-        best_ann = deepcopy(ann)
-    end
+    # Compute and store initial metrics
+    trainingLoss, _, validationLoss, _, _, _ = calculateMetrics()
 
-    # Return the network and the evolution of the error
-    if is_val_empty && is_test_empty
-        return (best_ann, trainingLosses)
-    elseif is_val_empty
-        return (best_ann, trainingLosses, testLosses)
-    elseif is_test_empty
-        return (best_ann, trainingLosses, validationLosses)
+    # Best model at validation set 
+    numEpochsValidation = 0
+    bestValidationLoss = validationLoss
+    
+    if (useValidation)
+        bestAnn = deepcopy(ann)
     else
-        return (best_ann, trainingLosses, validationLosses, testLosses)
+        bestAnn = ann  # if no validation, we want to return the ANN that is trained in every cycle
     end
+    
+    # Start the training
+
+    while (currentEpoch < maxEpochs) && (trainingLoss > minLoss) && (numEpochsValidation < maxEpochsVal)
+            
+        # Update epoch number
+        currentEpoch += 1
+
+        # Fit the model
+        Flux.train!(loss, Flux.params(ann), [(trainingInputs', trainingTargets')], ADAM(learningRate))
+
+        # Compute and store metrics
+        trainingLoss, _, validationLoss, _, _, _ = calculateMetrics()
+
+        # Update validation early stopping only if validation set given
+        if (useValidation)    
+            if (validationLoss < bestValidationLoss)
+                bestValidationLoss = validationLoss
+                numEpochsValidation = 0
+                bestAnn = deepcopy(ann)
+            else
+                numEpochsValidation += 1
+            end
+        end
+
+    end
+    
+    # Print stop reason and final metrics
+
+    if showText
+        println("Final results for epoch $currentEpoch: 
+            \n\tTraining loss: $(trainingLosses[end]), accuracy: $(100 * trainingAccs[end])%
+            \n\tValidation loss: $(validationLosses[end]), accuracy: $(100 * validationAccs[end])%
+            \n\tTest loss: $(testLosses[end]), accuracy: $(100 * testAccs[end])%")
+    
+        println("\nStopping criteria: 
+            \n\tMax. epochs: $(currentEpoch >= maxEpochs)
+            \n\tMin. loss: $(trainingLoss <= minLoss)
+            \n\tNum. epochs validation: $(numEpochsValidation >= maxEpochsVal)")
+    end
+    
+    return bestAnn, trainingLosses, validationLosses, testLosses, trainingAccs, validationAccs, testAccs
 end
 
 
@@ -443,4 +467,144 @@ end
 function confusionMatrix(outputs::AbstractArray{<:Real,1},targets::AbstractArray{Bool,1}; threshold::Real=0.5)
     outputs = outputs .> threshold
     return confusionMatrix(outputs, targets)
+end
+
+function oneVSall(inputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2})
+    numInstances, numClasses = size(targets)
+
+    # Create a 2-dimensional matrix of real values, with as many rows as patterns and as many columns as classes
+    outputs = Array{Float32,2}(undef, numInstances, numClasses)
+
+    # Make a loop that iterates over each class
+    for numClass in 1:numClasses
+        # Create the desired outputs corresponding to that class
+        desiredOutputs = targets[:, numClass]
+
+        # Train the corresponding model with those inputs and the new desired outputs corresponding to that class
+        model, _ = trainClassANN([2, 2], (inputs, desiredOutputs))
+
+        # Apply the model to the inputs to calculate the outputs, which will be copied into the previously created matrix
+        outputs[:,numClass] .= model(inputs')'
+    end
+    
+    vmax = maximum(outputs, dims=2);
+    outputs = (outputs .== vmax);
+
+    return outputs
+end
+
+function confusionMatrix(outputs::AbstractArray{Bool,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
+    @assert size(outputs) == size(targets)
+    @assert size(outputs, 2) > 1
+    
+    numClasses = size(outputs, 2)
+    sensitivity = zeros(numClasses)
+    specificity = zeros(numClasses)
+    precision = zeros(numClasses)
+    negative_predictive_value = zeros(numClasses)
+    fScore = zeros(numClasses)
+    confusion_matrix = zeros(Int, numClasses, numClasses)
+    
+    for i in 1:numClasses
+        classOutputs = outputs[:,i]
+        classTargets = targets[:,i]
+        if any(classTargets)
+            classMetrics = confusionMatrix(classOutputs, classTargets)
+            sensitivity[i] = classMetrics.sensitivity
+            specificity[i] = classMetrics.specificity
+            precision[i] = classMetrics.precision
+            negative_predictive_value[i] = classMetrics.negative_predictive_value
+            fScore[i] = classMetrics.fScore
+        end
+
+        for j in 1:numClasses
+            confusion_matrix[i, j] = sum(targets[:, i] .& outputs[:, j])
+        end
+    end
+
+    numInstancesFromEachClass = vec(sum(targets, dims=1))
+    
+    if weighted
+        weights = numInstancesFromEachClass ./ size(targets, 1)
+        sensitivity = sum(weights.*sensitivity)
+        specificity = sum(weights.*specificity)
+        precision = sum(weights.*precision)
+        negative_predictive_value = sum(weights.*negative_predictive_value)
+        fScore = sum(weights.*fScore)
+    else
+        sensitivity = mean(sensitivity)
+        specificity = mean(specificity)
+        precision = mean(precision)
+        negative_predictive_value = mean(negative_predictive_value)
+        fScore = mean(fScore)
+    end
+
+    acc = accuracy(targets, outputs)
+    errorRate = 1 - acc
+    
+    return (accuracy=acc,
+            errorRate=errorRate,
+            sensitivity=sensitivity,
+            specificity=specificity,
+            precision=precision,
+            negative_predictive_value=negative_predictive_value,
+            fScore=fScore,
+            confusion_matrix=confusion_matrix)
+end
+
+function confusionMatrix(outputs::AbstractArray{<:Real,2}, targets::AbstractArray{Bool,2}; weighted::Bool=true)
+    boolOutputs = classifyOutputs(outputs)
+    return confusionMatrix(boolOutputs, targets, weighted=weighted)
+end
+
+function confusionMatrix(outputs::AbstractArray{<:Any,1}, targets::AbstractArray{<:Any,1}; weighted::Bool=true)
+    @assert(all([in(output, unique(targets)) for output in outputs]))
+    
+    classes = unique(targets)
+    return confusionMatrix(oneHotEncoding(outputs,classes) ,oneHotEncoding(targets,classes));
+    
+    return confusionMatrix(boolOutputs, boolTargets, weighted=weighted)
+end
+
+# Define a function that performs k-fold cross-validation
+# N: the number of patterns
+# k: the number of folds
+function crossvalidation(N::Int64, k::Int64)
+    # Calculate the number of patterns in each fold
+    fold_size = ceil(Int, N / k)
+    # Repeat the indices of the folds k times
+    idx = repeat(1:k, fold_size)[1:N]
+    # Shuffle the indices randomly
+    shuffle!(idx)
+    # Return the shuffled indices
+    return idx
+end
+
+function crossvalidation(targets::AbstractArray{Bool,2}, k::Int64)
+    # Check that there are enough instances per class to perform k-fold cross-validation
+    @assert all(sum(targets, dims=1) .>= k) "There are not enough instances per class to perform a $(k)-fold cross-validation"
+    
+    # Initialize the index vector to zeros
+    idx = Int.(zeros(size(targets, 1)))
+    
+    # Iterate over the classes and perform stratified k-fold cross-validation
+    for class in 1:size(targets, 2)
+        # Find the indices of the patterns that belong to the current class
+        class_indices = findall(targets[:, class])
+        # Perform stratified k-fold cross-validation on the patterns that belong to the current class
+        class_folds = crossvalidation(length(class_indices), k)
+        # Update the index vector with the indices of the folds for the current class
+        idx[class_indices] .= class_folds
+    end
+    
+    # Return the index vector
+    return idx
+end;
+
+function crossvalidation(targets::AbstractArray{<:Any,1}, k::Int64)
+    # Convert the targets to a binary matrix using one-hot encoding
+    binary_targets = oneHotEncoding(targets)
+    # Perform stratified k-fold cross-validation on the binary matrix
+    idx = crossvalidation(binary_targets, k)
+    return idx
 end
