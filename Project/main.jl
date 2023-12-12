a2173f73919b1c05eee42114161ba96e71456be9
AlejandroDopico2 <alexdopico2001@gmail.com>
2023-12-12 14:20:25 +0100

    Update

---
+++ Project/main.jl
@@ -0,0 +1,105 @@
+using CSV
+using DataFrames
+using Random
+import StatsBase: countmap
+using ScikitLearn
+
+@sk_import svm:SVC;
+@sk_import tree:DecisionTreeClassifier;
+@sk_import neighbors: KNeighborsClassifier;
+@sk_import ensemble:StackingClassifier
+@sk_import ensemble:VotingClassifier
+
+
+# include("utils/split.jl");
+# include("utils/preprocess.jl");
+# include("utils/crossVal.jl");
+# include("utils/metrics.jl");
+# include("utils/ensemble.jl");
+# include("utils/ann.jl");
+# include("utils/finalModel.jl");
+# include("utils/multiclass.jl");
+
+include("utils.jl")
+
+function ip_to_decimal(ip)
+    # Split the IP address into octets
+    octets = split(ip, '.')
+    # Convert each octet to binary and combine them into a single 32-bit number
+    binary = join([string(parse(Int, octet, base=10), base=2, pad=8) for octet in octets])
+    decimal = parse(Int, binary, base=2) # Convert binary to decimal
+    return decimal
+end
+
+function generate_latex_table(metrics::Dict{String, <: Any}, final::Bool)
+    
+    topology = metrics["topology"]
+    accuracy = metrics["accuracy"]
+    recall = metrics["recall"]
+    specificity = metrics["specificity"]
+    f1_score = metrics["f1_score"]
+    
+    if final
+        confusion_matrix = metrics["confusion_matrix"]
+        println("$topology & $(round(accuracy*100, digits=2))\\%  & $(round(recall*100, digits=2))\\%  & $(round(specificity*100, digits=2))\\%  & $(round(f1_score*100, digits=2))\\% & $confusion_matrix \\\\")
+    else
+        std_accuracy = metrics["std_accuracy"]
+        std_recall = metrics["std_recall"]
+        std_specificity = metrics["std_specificity"]
+        std_f1_score = metrics["std_f1_score"]
+        println("$topology & $(round(accuracy*100, digits=2))\\% \\textit{($(round(std_accuracy, digits = 2)))} & $(round(recall*100, digits=2))\\% \\textit{($(round(std_recall, digits = 2)))} & $(round(specificity*100, digits=2))\\% \\textit{($(round(std_specificity, digits = 2)))} & $(round(f1_score*100, digits=2))\\% \\textit{($(round(std_f1_score, digits = 2)))} \\\\")
+    end
+    
+end
+
+file_path = "datasets/super_simplified_Android_Malware.csv";
+data = CSV.File(file_path, header=true) |> DataFrame;
+
+columns_to_drop = ["Flow ID", " Timestamp"]
+columns = names(data)
+
+println("Size of dataframe before dropping columns $(size(data))")
+for column in 1:size(data, 2)
+    unique_values = countmap(data[:, column])
+
+    if length(unique_values) == 1
+        println("Adding column $(columns[column])")
+        # println(unique_values)
+        push!(columns_to_drop, columns[column])
+    end
+    
+end
+
+select!(data, Not(columns_to_drop))
+
+println("Size of dataframe after dropping columns $(size(data))")
+
+dropmissing!(data)
+
+println("Size of dataframe after dropping nulls $(size(data))")
+
+unique_data = unique(data)
+
+println("Size of dataframe after dropping duplicating rows $(size(data))")
+
+println(countmap(data[:, :Label]));
+
+source_ips = data[!, :" Source IP"];
+destination_ips = data[!, :" Destination IP"];
+
+data[!, :"Source IP Decimal"] = map(ip -> ip_to_decimal(ip), source_ips);
+data[!, :"Destination IP Decimal"] = map(ip -> ip_to_decimal(ip), destination_ips);
+
+select!(data, Not([" Source IP", " Destination IP"]));
+
+output_data = data[!, :Label];
+select!(data, Not(:Label));
+input_data = Matrix(data[!, 1:size(data, 2)]);
+
+println("Size of the input dataset is $(size(input_data))")
+
+# println("Doing first approach")
+# include("first_approach.jl")
+
+println("Doing second approach")
+include("second_approach.jl")
---
+++ Project/second_approach.jl
@@ -0,0 +1,190 @@
+# Binary classification
+using MLDataPattern;
+
+function transform_binary_class(output_data)
+    binary_labels = output_data .!= "Benign"
+    return binary_labels
+end
+
+Random.seed!(42)
+
+binary_labels = transform_binary_class(output_data)
+@assert binary_labels isa BitVector
+@assert input_data isa Matrix
+
+X_bal, y_bal = oversample((input_data', binary_labels), shuffle = true)
+X_bal = getobs(X_bal)'
+y_bal = getobs(y_bal)
+
+println("Distribution of categories of second approach: $(countmap(y_bal))")
+
+# Preprocessing
+
+train_indexes, test_indexes = holdOut(size(X_bal, 1), 0.2)
+
+train_input = convert(Array{Float32, 2}, X_bal[train_indexes, :])
+train_balanced_binary_output = y_bal[train_indexes]
+
+standarizationParameters = calculateZeroMeanNormalizationParameters(train_input)
+
+normalizeZeroMean!(train_input, standarizationParameters)
+
+test_input = convert(Array{Float32, 2}, X_bal[test_indexes, :])
+test_balanced_binary_output = y_bal[test_indexes]
+
+normalizeZeroMean!(test_input, standarizationParameters)
+
+@assert size(test_input, 1) == size(test_balanced_binary_output, 1)
+@assert size(train_input, 1) == size(train_balanced_binary_output, 1)
+
+kFolds = 10
+crossValidationIndexes = crossvalidation(train_balanced_binary_output, kFolds);
+
+println("-------------------------Artificial Neural Networks---------------------------------------")
+
+topologies = [[20], [40], [80], [100], [60, 120], [80, 50], [80, 100], [100, 40]]
+
+annParameters = Dict("modelType" => :ANN, "maxEpochs" => 200,
+    "learningRate" => 0.01, "maxEpochsVal" => 30,
+    "repetitions" => 30, "validationRatio" => 0.1,
+    "transferFunctions" => fill(σ, 2))
+
+for topology in topologies
+    annParameters["topology"] = topology
+    metricsCV = modelCrossValidation(annParameters["modelType"], annParameters, train_input, train_balanced_binary_output, crossValidationIndexes)
+    metricsCV["topology"] = topology 
+
+    generate_latex_table(metricsCV, false)
+end
+
+println("----------------------------------------------------------------")
+
+for topology in topologies
+    annParameters["topology"] = topology
+    metrics = createAndTrainFinalModel(annParameters["modelType"], annParameters, train_input, train_balanced_binary_output, test_input, test_balanced_binary_output)
+    metrics["topology"] = topology 
+
+    generate_latex_table(metrics, true)
+end
+println("-------------------------kNN---------------------------------------")
+
+knnParameters = Dict("modelType" => :kNN, "numNeighboors" => 0)
+
+ks = [3 , 5, 7, 10, 15, 20]
+for k in ks
+    knnParameters["numNeighboors"] = k
+    metricsCV = (modelCrossValidation(knnParameters["modelType"], knnParameters, train_input, train_balanced_binary_output, crossValidationIndexes))
+    metricsCV["topology"] = k
+
+    generate_latex_table(metricsCV, false)
+end
+
+println("----------------------------------------------------------------")
+for k in ks
+    knnParameters["numNeighboors"] = k
+    metrics = createAndTrainFinalModel(knnParameters["modelType"], knnParameters, train_input, train_balanced_binary_output, test_input, test_balanced_binary_output)
+    metrics["topology"] = k
+
+    generate_latex_table(metrics, true)
+end
+
+println("-------------------------Decision Tree---------------------------------------")
+
+dtParameters = Dict("modelType" => :DecisionTree, "maxDepth" => 1)
+
+depths = [3, 5, 7, 10, 15, nothing]
+for depth in depths
+    dtParameters["maxDepth"] = depth
+    metricsCV = (modelCrossValidation(dtParameters["modelType"], dtParameters, train_input, train_balanced_binary_output, crossValidationIndexes))
+    metricsCV["topology"] = depth
+
+    generate_latex_table(metricsCV, false)
+
+end
+
+println("----------------------------------------------------------------")
+
+for depth in depths
+    dtParameters["maxDepth"] = depth
+    metrics = createAndTrainFinalModel(dtParameters["modelType"], dtParameters, train_input, train_balanced_binary_output, test_input, test_balanced_binary_output)
+    metrics["topology"] = depth
+
+    generate_latex_table(metrics, true)
+end
+
+println("-------------------------SVM---------------------------------------")
+
+svmParameters = Dict("modelType" => :SVM, "C" => 1, "kernel" => "linear", "degree" => 3, "gamma" => "scale")
+
+svms = [
+    ("rbf", 0.1),
+    ("rbf", 1.0),
+    ("rbf", 10.0),
+    ("poly", 0.1),
+    ("poly", 1.0),
+    ("linear", 0.1),
+    ("linear", 1.0),
+    ("linear", 10.0),
+]
+
+for (kernel, C) in svms
+    svmParameters["kernel"] = kernel
+    svmParameters["C"] = C
+    metricsCV = (modelCrossValidation(svmParameters["modelType"], svmParameters, train_input, train_balanced_binary_output, crossValidationIndexes))
+    metricsCV["topology"] = kernel * " & " * string(C)
+
+    generate_latex_table(metricsCV, false)
+
+end
+
+println("----------------------------------------------------------------")
+
+for (kernel, C) in svms
+    svmParameters["kernel"] = kernel
+    svmParameters["C"] = C
+    metrics = createAndTrainFinalModel(svmParameters["modelType"], svmParameters, train_input, train_balanced_binary_output, test_input, test_balanced_binary_output)
+    metrics["topology"] = kernel * " & " * string(C)
+
+    generate_latex_table(metrics, true)
+
+end
+
+println("-------------------------Ensembles---------------------------------------")
+
+
+dtParameters = Dict("modelType" => :DecisionTree, "maxDepth" => nothing)
+knnParameters = Dict("modelType" => :kNN, "numNeighboors" => 3)
+svmParameters = Dict("modelType" => :SVM, "kernel" => "rbf", "C" => 10)
+Random.seed!(42)
+
+ensemble_types = [:VotingHard, :Stacking]
+final_estimators = [dtParameters, knnParameters, svmParameters]
+
+for ensemble_type in ensemble_types
+    for final_estimator in final_estimators
+        metricsCV = trainClassEnsemble([:DecisionTree, :kNN, :SVM], [dtParameters, knnParameters, svmParameters], (train_input, train_balanced_binary_output), crossValidationIndexes; ensembleType = ensemble_type, final_estimator = final_estimator)
+        metricsCV["topology"] = final_estimator
+        generate_latex_table(metricsCV, false)
+
+        if ensemble_type == :VotingHard
+            break
+        end
+
+    end
+end
+
+println("----------------------------------------------------------------")
+
+
+for ensemble_type in ensemble_types
+    for final_estimator in final_estimators
+        metrics = createAndTrainFinalEnsemble([:DecisionTree, :kNN, :SVM], [dtParameters, knnParameters, svmParameters], (train_input, train_balanced_binary_output), (test_input, test_balanced_binary_output); ensembleType = ensemble_type, final_estimator = final_estimator)
+        metrics["topology"] = final_estimator
+        generate_latex_table(metrics, true)
+
+        if ensemble_type == :VotingHard
+            break
+        end
+    end
+end
+
